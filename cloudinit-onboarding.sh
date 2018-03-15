109fa946b8399ed22539c8d693deec7d9bc8f97f
yuanfmjohn <37332335+yuanfmjohn@users.noreply.github.com>
2018-03-15 13:24:14 +0700

    Update cloudinit-onboarding.sh

--- cloudinit-onboarding.sh
+++ cloudinit-onboarding.sh
@@ -15,7 +15,6 @@ mkdir -p /config/cloud/f5-cloud-libs
 curl -o /config/cloud/f5-cloud-libs.tar.gz --silent --fail --retry 10 -L https://raw.githubusercontent.com/f5Networks/f5-cloud-libs/v3.5.0/dist/f5-cloud-libs.tar.gz
 tar zxvf /config/cloud/f5-cloud-libs.tar.gz --strip 1 -C /config/cloud/f5-cloud-libs
 cd /config/cloud/f5-cloud-libs
-npm install --production
 sleep 5
 f5-rest-node /config/cloud/f5-cloud-libs/scripts/onboard.js -o /var/log/onboard.log --no-reboot --host $mgmt_ip --user admin --password admin --set-root-password old:default,new:root --update-user user:admin,password:admin --ntp 0.jp.pool.ntp.org --tz Asia/Shanghai --dns 8.8.8.8 --module ltm:nominal --license-pool --big-iq-host 10.0.0.60 --big-iq-user admin --big-iq-password admin --license-pool-name ve-pool
 f5-rest-node /config/cloud/f5-cloud-libs/scripts/network.js -o /var/log/onboard-network.log --host $mgmt_ip --user admin --password admin --vlan name:vlan_ha,nic:1.1 --self-ip name:self_ha,address:192.168.177.110/24,vlan:vlan_ha --vlan name:vlan_in,nic:1.2 --self-ip name:self_in,address:192.168.125.110/24,vlan:vlan_in --vlan name:vlan_out,nic:1.3 --self-ip name:self_out,address:192.168.152.110/24,vlan:vlan_out --route name:default_gw,gw:192.168.152.1,network:0.0.0.0/0
